7eba8db5eccc06faac28e774edd490c33d8a3402
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-09-05 16:17:23 -0500

    Create Splash controller, helper and view.

---
+++ app/controllers/splash_controller.rb
@@ -0,0 +1,7 @@
+class SplashController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    # implement
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'splash#index'
 
   devise_for :users, controllers: {
     sessions: 'users/sessions'
