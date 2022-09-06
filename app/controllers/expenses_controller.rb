bf792f0b8ab01b713eca564a8a6d914ac3c65f7d
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-09-06 07:13:21 -0500

    Implement controllers to pass controller tests

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,10 +1,29 @@
 class ExpensesController < ApplicationController
+  def create
+    if @expense.save
+      flash[:notice] = 'Expense saved successfully'
+    else
+      flash[:alert] = 'Expense not saved'
+    end
+    redirect_to group_expenses_path(group_id: params[:group_id])
+  end
+
   def index
+    current_user.expenses
   end
 
   def new
+    # implement
   end
 
   def show
+    # implement
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).merge({ user_id: current_user.id })
+      .permit(:user_id, :name, :amount)
   end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,29 @@
 class GroupsController < ApplicationController
+  def create
+    if @group.save
+      flash[:notice] = 'Category saved successfully'
+    else
+      flash[:alert] = 'Category not saved'
+    end
+    redirect_to groups_path
+  end
+
   def index
+    # implement
   end
 
   def new
+    # implement
+  end
+
+  def show
+    # implement
+  end
+
+  private
+
+  def group_params
+    params.require(:group).merge({ user_id: current_user.id })
+      .permit(:user_id, :name, :icon)
   end
 end
