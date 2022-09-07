1f17566fc5cb6eb65a16cf2e0dfaf4e4c158a599
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-09-07 12:31:23 -0500

    Modifiy configuration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -264,6 +264,7 @@ Devise.setup do |config|
   #
   # The "*/*" below is required to match Internet Explorer requests.
   # config.navigational_formats = ['*/*', :html]
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
